d7e66e1f6c7aa80e881e224192157d745b594369
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-03-26 17:14:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,9 +22,9 @@ end
 def now_serving(name)
   if katz_deli.length == 0
   puts "There is nobody waiting to be served!"
-#elsif katz_deli.length >= 1
-#  katz_deli.shift
-#  return  "#{katz_deli[0]}"
+elsif katz_deli.length >= 1
+puts "Currently serving #{katz_deli[0]}"
+katz_deli.shift
 
 end
 end
