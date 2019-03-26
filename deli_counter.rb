ba272d1cdc063ded8e46521a686369d19bd0ed45
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-03-26 17:11:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,12 +17,14 @@ def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
-#def now_serving(name)
-  #if katz_deli.length == 0
-  #return "There is nobody waiting to be served!"
+
+
+def now_serving(name)
+  if katz_deli.length == 0
+  puts "There is nobody waiting to be served!"
 #elsif katz_deli.length >= 1
 #  katz_deli.shift
 #  return  "#{katz_deli[0]}"
 
-#end
-#end
+end
+end
