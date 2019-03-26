b717f79e98079eddfca4f121c59a0e4abc535c2a
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-03-26 17:02:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,15 +13,21 @@ end
 end
 
 
-def take_a_number(name, array)
-if katz_deli.length == 0
-  katz_deli.collect do |new_customer|
-      katz_deli << "Ada"
+def take_a_number(current_line, new_person)
+  current_line << new_person
+  current_line.length
 end
-puts "Welcome, #{katz_deli}. You are number #{array.to_i + 1} in line."
 
-end
-end
+
+#def take_a_number(name, array)
+#if katz_deli.length == 0
+#  katz_deli.collect do |new_customer|
+#      katz_deli << "Ada"
+#end
+#puts "Welcome, #{katz_deli}. You are number #{array.to_i + 1} in line."
+
+#end
+#end
 
 
 
